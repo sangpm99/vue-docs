abba73b29cb7b42b21e1ba6c49ac7ccf8583019c
sangpm <sangpm72@wru.vn>
2025-01-09 11:46:25 +0700

    Updated

---
+++ index.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script setup lang="ts">
+import { useAbility } from "@casl/vue";
+
+// Configs
+const { can } = useAbility();
+
+definePage({
+  meta: {
+    action: "Read",
+    subject: "Permission.Public",
+  },
+});
+
+</script>
