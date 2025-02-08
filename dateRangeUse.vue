6e01ce88a17d87b49b861a758c9e0b8b9ec4c8f3
sangpm <sangpm72@wru.vn>
2025-02-08 09:47:23 +0700

    Updated

---
+++ dateRangeUse.vue
@@ -0,0 +1,28 @@
+<template>
+    <DateRangeSelect
+        v-model:from="from"
+        v-model:to="to"
+        :key="remount"
+    ></DateRangeSelect>
+</template>
+
+<script setup lang="ts">
+import DateRangeSelect from "@/components/selects/DateRangeSelect.vue";
+
+const from: Ref<number | null> = ref(null);
+const to: Ref<number | null> = ref(null);
+const remount = ref<number>(0);
+
+const get = async () => {
+  searchQuery.value.from = from.value;
+  searchQuery.value.to = to.value;
+  // api
+};
+
+const clear = () => {
+  from.value = null;
+  to.value = null;
+  get();
+  remount.value++;
+};
+</script>
--- tiny.vue
+++ tiny.vue
@@ -1,4 +1,7 @@
-<label>Description *</label>
+<label>Description <span class="text-error">*</span></label>
 <TinyTextEditor
   v-model="woocommerceTemplate.properties.description"
 ></TinyTextEditor>
+
+import { requiredValidator } from "@/@core/utils/validators";
+:rules="[requiredValidator]"
